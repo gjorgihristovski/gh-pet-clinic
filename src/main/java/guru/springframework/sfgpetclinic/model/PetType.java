a0a8251cedc0eb57648a75ce6473da17f3813f34
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-10-29 13:21:46 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * Created by gjorgi.hristovski on 29-Oct-18 in 13:18
+ * Created in guru.springframework.sfgpetclinic.model for sfg-pet-clinic
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

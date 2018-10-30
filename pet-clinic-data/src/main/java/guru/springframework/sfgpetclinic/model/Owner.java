2dc6a5235003dc0018ab8ea6bf5a314da7067766
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-10-30 08:07:55 +0100

    release plugin testing

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
+/**
+ * Created by gjorgi.hristovski on 29-Oct-18 in 13:17
+ * Created in guru.springframework.sfgpetclinic.model for sfg-pet-clinic
+ */
 public class Owner extends Person {
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 /**
- *
+ * Created by gjorgi.hristovski on 29-Oct-18 in 13:17
+ * Created in guru.springframework.sfgpetclinic.model for sfg-pet-clinic
  */
 public class Person {
 

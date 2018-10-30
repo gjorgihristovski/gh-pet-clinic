60a855ccd429343d10c7e5b24b52b4892c0f7e45
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-10-30 10:39:33 +0100

    Refactor to CrudRepository. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -2,16 +2,10 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
 /**
  * Created by gjorgi.hristovski on 30-Oct-18 in 08:18
  * Created in guru.springframework.sfgpetclinic.services for sfg-pet-clinic
  */
-public interface PetService {
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,16 +2,10 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
 /**
  * Created by gjorgi.hristovski on 30-Oct-18 in 08:19
  * Created in guru.springframework.sfgpetclinic.services for sfg-pet-clinic
  */
-public interface VetService {
-    Vet findById(Long id);
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }

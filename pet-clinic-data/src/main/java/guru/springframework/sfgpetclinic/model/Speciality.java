84f9b521021d741737648ca9b7de103292561b9e
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-11-06 10:19:00 +0100

    Created Vets Entity for JPA Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by gjorgi.hristovski on 02-Nov-18 in 13:51
  * Created in guru.springframework.sfgpetclinic.model for sfg-pet-clinic
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description;

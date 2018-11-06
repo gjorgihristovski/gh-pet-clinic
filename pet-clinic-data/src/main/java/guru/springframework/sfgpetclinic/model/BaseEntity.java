a0b478d6a3e3200c76efa08557ea0e1a60187562
Gjorgi Hristovski <gjorgi.hristovski@mail.com>
2018-11-06 09:47:41 +0100

    Update base entity for JPA Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by gjorgi.hristovski on 30-Oct-18 in 08:24
  * Created in guru.springframework.sfgpetclinic.model for sfg-pet-clinic
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
